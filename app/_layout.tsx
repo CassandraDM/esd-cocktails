2220a6152c1b478abdef25b6f463dc7c4c8af3e6
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-11-18 14:43:47 +0100

    set status bar to black

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,52 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="counter/index" options={{ title: "Counter" }} />
-      <Stack.Screen
-        name="cocktails-list/index"
-        options={{ title: "Cocktails List" }}
-      />
-    </Stack>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="black" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerTintColor: "pink",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="counter/index"
+          options={{
+            title: "Counter",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerTintColor: "pink",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="cocktails-list/index"
+          options={{
+            title: "Cocktails List",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerTintColor: "pink",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack>
+    </>
   );
 }
 
